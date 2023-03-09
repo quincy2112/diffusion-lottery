9d6fea031e4099c4e7d5015a95b465f75fa5b6a0
Vikram Duvvur <vduvvur@andrew.cmu.edu>
2023-03-08 20:40:22 -0500

    LTH code

--- train.py
+++ train.py
@@ -33,20 +33,10 @@ n_inference_timesteps = 50
 
 
 def main(args):
+    # Setup Code
     timestamp = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-    model = UNet(3, image_size=args.resolution, hidden_dims=[16, 32, 64, 128])
+    
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    noise_scheduler = DDIMScheduler(num_train_timesteps=n_timesteps,
-                                    beta_schedule="cosine", device=device)
-
-    optimizer = torch.optim.AdamW(
-        model.parameters(),
-        lr=args.learning_rate,
-        betas=(args.adam_beta1, args.adam_beta2),
-        weight_decay=args.adam_weight_decay,
-        eps=args.adam_epsilon,
-    )
 
     augmentations = Compose([
         Resize(args.resolution, interpolation=InterpolationMode.BILINEAR),
@@ -84,6 +74,116 @@ def main(args):
                                                     )
     real_image_samples = next(iter(train_dataloader))['input']
 
+    # LTH Code    
+    prune_percent_per_LTH_round = 1 - (1 - args.LTH_prune_percent)**(1/args.LTH_rounds)
+    print("Pruning", str(round(prune_percent_per_LTH_round, 4)*100) + "% for a total prune percent of", str(args.LTH_prune_percent*100) + "% over", args.LTH_rounds, "rounds")
+    
+    model = UNet(3, image_size=args.resolution, hidden_dims=[16, 32, 64, 128])
+    model = model.to(device)
+
+    mask = dict()
+    init_weights = dict()
+    
+    for name, param in model.named_parameters():
+        mask[name] = torch.ones(param.shape).to(device)
+        init_weights[name] = param.detach().clone().to(device)
+    
+        
+    for rnd in range(args.LTH_rounds):
+        print("LTH round:", rnd)
+        
+        # Copy masked initial weights to model
+        with torch.no_grad():
+            for name, param in model.named_parameters():
+                param.copy_(mask[name]*init_weights[name])
+
+        model.train()
+        noise_scheduler = DDIMScheduler(num_train_timesteps=n_timesteps,
+                                        beta_schedule="cosine", device=device)
+
+        optimizer = torch.optim.AdamW(
+            model.parameters(),
+            lr=args.learning_rate,
+            betas=(args.adam_beta1, args.adam_beta2),
+            weight_decay=args.adam_weight_decay,
+            eps=args.adam_epsilon,
+        )
+    
+        lr_scheduler = get_scheduler(
+            args.lr_scheduler,
+            optimizer=optimizer,
+            num_warmup_steps=args.lr_warmup_steps,
+            num_training_steps=(len(train_dataloader) * args.num_epochs) //
+            args.gradient_accumulation_steps,
+        )
+
+        for epoch in range(args.LTH_epochs_per_rounds):
+            for step, batch in enumerate(train_dataloader):
+                progress_bar = tqdm(total=len(train_dataloader))
+                progress_bar.set_description(f"Epoch {epoch}")
+                
+                optimizer.zero_grad()
+                clean_images = batch["input"].to(device)
+                clean_images = normalize_to_neg_one_to_one(clean_images)
+
+                batch_size = clean_images.shape[0]
+                noise = torch.randn(clean_images.shape).to(device)
+                timesteps = torch.randint(0, noise_scheduler.num_train_timesteps, 
+                                         (batch_size,), device=device).long()
+                noisy_images = noise_scheduler.add_noise(clean_images, noise, timesteps)
+
+                noise_pred = model(noisy_images, timesteps)["sample"]
+                loss = F.l1_loss(noise_pred, noise)
+                loss.backward()
+
+                if args.use_clip_grad:
+                    clip_grad_norm_(model.parameters(), 1.0)
+                    
+                # Apply our 0/1 mask
+                for name, param in model.named_parameters():
+                    param.grad *= mask[name]
+                    
+                optimizer.step()
+                lr_scheduler.step()
+                
+                progress_bar.update(1)
+            progress_bar.close()
+        # Identify threshold and update mask
+        all_weights = list()
+        
+        for name, param in model.named_parameters():
+            filtered_params = torch.abs(param*mask[name])
+            all_weights.extend(filtered_params.flatten().tolist())
+                
+        sorted_arr = sorted(all_weights)
+        while sorted_arr[0] == 0:
+            sorted_arr.pop(0)
+            
+        threshold = sorted_arr[int(len(sorted_arr)*prune_percent_per_LTH_round)]
+            
+        for name, param in model.named_parameters():
+            mask[name] = mask[name]*(torch.abs(param) > threshold).int()
+        
+    # Actual Model training code
+    model = UNet(3, image_size=args.resolution, hidden_dims=[16, 32, 64, 128])
+
+    # Copy masked initial weights to model
+    with torch.no_grad():
+        for name, param in model.named_parameters():
+            param.copy_(mask[name]*init_weights[name])
+
+    noise_scheduler = DDIMScheduler(num_train_timesteps=n_timesteps,
+                                    beta_schedule="cosine", device=device)
+
+    optimizer = torch.optim.AdamW(
+        model.parameters(),
+        lr=args.learning_rate,
+        betas=(args.adam_beta1, args.adam_beta2),
+        weight_decay=args.adam_weight_decay,
+        eps=args.adam_epsilon,
+    )
+
+    
     lr_scheduler = get_scheduler(
         args.lr_scheduler,
         optimizer=optimizer,
@@ -95,7 +195,7 @@ def main(args):
     if not os.path.exists("logs"):
         os.mkdir("logs")
         
-    log_filename = "logs/log_" + str(time.time_ns() // 1_000_000) + ".csv"
+    log_filename = "logs/" +str(args.LTH_prune_percent) + "_log_" + str(time.time_ns() // 1_000_000) + ".csv"
     with open(log_filename, "a") as file:
         file.write("epoch,fid,train_loss\n")
 
@@ -125,6 +225,12 @@ def main(args):
 
             if args.use_clip_grad:
                 clip_grad_norm_(model.parameters(), 1.0)
+    
+            # Apply our 0/1 mask
+            for name, param in model.named_parameters():
+                param.grad *= mask[name]
+                
+                    
             optimizer.step()
             lr_scheduler.step()
             optimizer.zero_grad()
@@ -180,20 +286,20 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Simple example of a training script.")
-    parser.add_argument("--dataset_name", type=str, default=None)
+    parser.add_argument("--dataset_name", type=str, default="flowers")
     parser.add_argument("--dataset_config_name", type=str, default=None)
     parser.add_argument("--train_data_dir",
                         type=str,
                         default=None,
                         help="A folder containing the training data.")
     parser.add_argument("--output_dir", type=str, default="trained_models/ddpm-model-64.pth")
-    parser.add_argument("--samples_dir", type=str, default="test_samples/")
-    parser.add_argument("--loss_logs_dir", type=str, default="training_logs")
+    parser.add_argument("--samples_dir", type=str, default="replica_samples/")
+    parser.add_argument("--loss_logs_dir", type=str, default="replica_training_logs")
     parser.add_argument("--cache_dir", type=str, default=None)
     parser.add_argument("--resolution", type=int, default=64)
-    parser.add_argument("--train_batch_size", type=int, default=16)
+    parser.add_argument("--train_batch_size", type=int, default=64)
     parser.add_argument("--eval_batch_size", type=int, default=32)
-    parser.add_argument("--num_epochs", type=int, default=100)
+    parser.add_argument("--num_epochs", type=int, default=2000)
     parser.add_argument("--save_model_epochs", type=int, default=10)
     parser.add_argument("--gradient_accumulation_steps", type=int, default=1)
     parser.add_argument("--learning_rate", type=float, default=1e-3)
@@ -201,12 +307,15 @@ if __name__ == "__main__":
     parser.add_argument("--lr_warmup_steps", type=int, default=500)
     parser.add_argument("--adam_beta1", type=float, default=0.95)
     parser.add_argument("--adam_beta2", type=float, default=0.999)
-    parser.add_argument("--adam_weight_decay", type=float, default=1e-5)
+    parser.add_argument("--adam_weight_decay", type=float, default=0)
     parser.add_argument("--adam_epsilon", type=float, default=1e-08)
     parser.add_argument("--use_clip_grad", type=bool, default=False)
     parser.add_argument("--logging_dir", type=str, default="logs")
 
-
+    parser.add_argument("--LTH_rounds", type=int, default=5)
+    parser.add_argument("--LTH_epochs_per_rounds", type=int, default=10)
+    parser.add_argument("--LTH_prune_percent", type=float, default=0.2)
+    
     args = parser.parse_args()
 
     if args.dataset_name is None and args.train_data_dir is None:
